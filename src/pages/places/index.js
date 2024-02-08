710b2b6c9af29521a220f3cbfba0456edbc75099
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2024-02-08 13:20:00 -0300

    Feat: ajustando tela de places

--- src/pages/places/index.js
+++ src/pages/places/index.js
@@ -1,5 +1,6 @@
 import { Button } from "../../components/ui/button"
 import image from "../../assets/logoDC.svg"
+import lugares from "../../assets/Mapa Piso Terreo DC 2022.jpg"
 import {Info, Computer, MonitorCheck, Presentation, Droplets, LibraryBig, ChevronLeft} from 'lucide-react';
 
 
@@ -15,60 +16,10 @@ export default function Places() {
         <div className="text-black text-8xl font-normal mb-6 text-center">
           Lugares
         </div>
-        <div className="text-center text-black text-2xl font-normal w-[60%] 2xl:w-[50%] my-6">
-          Estes são os lugares do departamento, selecione um para mais informações
+        <div className="text-center text-black text-2xl font-normal my-6">
+          Estes são os lugares do departamento
         </div>
-        <div className="text-center text-black text-2xl font-bold my-6">
-          Selecione uma opção
-        </div>
-        <div className="flex flex-row gap-6 mb-6">
-          <a href="/places">
-            <Button className="w-[22rem]">
-              Banheiros
-              <Droplets size="28px" />
-            </Button>
-          </a>
-          <a href="/places">
-            <Button className="w-[22rem]">
-              LIG
-              <Computer size="28px" />
-            </Button>
-          </a>
-          <a href="/places">
-            <Button className="w-[22rem]">
-              Secretaria
-              <Info size="28px" />
-            </Button>
-          </a>
-        </div>
-        <div className="flex flex-row gap-6 mb-12">
-          <a href="/places">
-            <Button className="w-[22rem]">
-              Auditório
-              <Presentation size="28px" />
-            </Button>
-          </a>
-          <a href="/places">
-            <Button className="w-[22rem]">
-              Laboratórios
-              <MonitorCheck size="28px" />
-            </Button>
-          </a>
-          <a href="/places">
-            <Button className="w-[22rem]">
-              Professores
-              <LibraryBig size="28px" />
-            </Button>
-          </a>
-        </div>
-        <div className="">
-          <a href="/">
-            <Button className="w-[22rem] justify-start gap-24">
-              <ChevronLeft size="28px" />
-              Voltar
-            </Button>
-          </a>
-        </div>
+          <img src={lugares} alt="locais" className="" style={{transform: "rotate(90deg)"}} width={600}/>
       </div>
 
       <div className="z-0 w-[20%] 2xl:w-[25%] flex flex-col justify-between items-center p-4 absolute h-full right-0">
