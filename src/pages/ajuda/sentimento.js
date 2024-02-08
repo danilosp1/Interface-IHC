87a4ef2ee88fd8c36ec1d567cb7dad0ae5c8c813
Danilo da Silva Pinto <danilo.sp03@gmail.com>
2024-02-08 16:39:50 -0300

    Feat: adicionado json com emoções e configuração base da tela de ajuda

---
+++ src/pages/ajuda/sentimento.js
@@ -0,0 +1,106 @@
+const sentimentoText = {
+    "1": {
+        "texto": "triste",
+        "def": "tristeza",
+        "fisicos": [
+            "Choro frequente",
+            "Fadiga",
+            "Distúrbios de sono",
+            "Dores no corpo"
+        ],
+        "emocionais": [
+            "Sentimento de vazio",
+            "Desânimo",
+            "Falta de prazer",
+            "Baixa autoestima"
+        ],
+        "cognitivos": [
+            "Dificuldade de concentração",
+            "Pensamentos negativos",
+            "Autoavaliação negativa"
+        ],
+        "sociais": [
+            "Isolamento social",
+            "Dificuldade em interagir",
+            "Mudanças no apetite"
+        ]
+    },
+    "2": {
+        "texto": "feliz",
+        "def": "felicidade",
+        "fisicos": [
+            "Sensação de leveza",
+            "Sorriso frequente",
+            "Aumento de energia",
+            "Bem-estar geral"
+        ],
+        "emocionais": [
+            "Contentamento",
+            "Entusiasmo",
+            "Satisfação",
+            "Alegria intensificada"
+        ],
+        "cognitivos": [
+            "Pensamentos positivos",
+            "Clareza mental",
+            "Foco e concentração"
+        ],
+        "sociais": [
+            "Socialização ativa",
+            "Empatia",
+            "Compartilhamento de conquistas"
+        ]
+    },
+    "3": {
+        "texto": "com raiva",
+        "def": "raiva",
+        "fisicos": [
+            "Aumento da frequência cardíaca",
+            "Tensão muscular",
+            "Respiração acelerada",
+            "Punhos cerrados"
+        ],
+        "emocionais": [
+            "Sentimento de irritação",
+            "Frustração intensa",
+            "Hostilidade",
+            "Impaciência"
+        ],
+        "cognitivos": [
+            "Pensamentos negativos",
+            "Dificuldade de raciocínio lógico",
+            "Foco estreitado"
+        ],
+        "sociais": [
+            "Isolamento social",
+            "Comportamento agressivo",
+            "Dificuldade de comunicação"
+        ]
+    },
+    "4": {
+        "texto": "confuso",
+        "def": "confusão",
+        "fisicos": [
+            "Sensação de desconforto",
+            "Inquietação",
+            "Variações na energia"
+        ],
+        "emocionais": [
+            "Confusão emocional",
+            "Dificuldade em nomear sentimentos",
+            "Flutuações de humor"
+        ],
+        "cognitivos": [
+            "Pensamentos confusos",
+            "Falta de clareza mental",
+            "Reflexões ambíguas"
+        ],
+        "sociais": [
+            "Retraimento social",
+            "Dificuldade em expressar emoções",
+            "Comportamento reservado"
+        ]
+    }
+}
+
+export default sentimentoText;
